5865f9963bb0817b1e07314b17f937920fe546ff
Michael Alexander <beefsack@gmail.com>
2015-07-26 21:09:52 +1000

    Implemented working interactivity with auth form.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 import Html
 import Router exposing (..)
 import History
+import StartApp
 
 import View.Auth
 import View.Home
@@ -9,10 +10,13 @@ import View.Games
 main : Signal Html.Html
 main = Signal.map route History.hash
 
+actions = Signal.mailbox Nothing
+address = Signal.forwardTo actions.address Just
+
 route : Route Html.Html
 route = match
   [ ""       :-> View.Home.display
-  , "#auth"  :-> View.Auth.display
+  , "#auth"  :-> \url -> View.Auth.view address View.Auth.init
   , "#games" :-> View.Games.display
   ] display404
 
--- src/View/Auth.elm
+++ src/View/Auth.elm
@@ -3,22 +3,89 @@ module View.Auth where
 import StartApp
 import Html
 import Html.Attributes
+import Html.Events
+import Json.Decode
 
-main = StartApp.start { model = "", view = view, update = update }
+main : Signal Html.Html
+main = StartApp.start { model = init, view = view, update = update }
 
-update newStr oldStr = newStr
+type alias Model =
+  { email : String
+  , confirmation : String
+  , sending : Bool
+  }
 
-view : Signal.Address String -> String -> Html.Html
-view address string =
-  Html.form []
-    [ Html.div [] [ Html.input
-      [ Html.Attributes.placeholder "Email address"
-      , Html.Attributes.value ""
+init : Model
+init =
+  { email = ""
+  , confirmation = ""
+  , sending = False
+  }
+
+type Action
+  = Submit
+  | UpdateEmail String
+  | UpdateConfirmation String
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Submit ->
+      { model |
+        sending <- True
+      }
+    UpdateEmail newEmail ->
+      { model |
+        email <- newEmail
+      }
+    UpdateConfirmation newConfirmation ->
+      { model |
+        confirmation <- newConfirmation
+      }
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  Html.form 
+    [ Html.Events.onWithOptions
+      "submit"
+      { stopPropagation = False, preventDefault = True }
+      Json.Decode.value
+      (\_ -> Signal.message address Submit)
+    ]
+    [ Html.div []
+      [ Html.input
+        [ Html.Attributes.class "mdl-textfield__input"
+        , Html.Attributes.placeholder "Email address"
+        , Html.Attributes.value model.email
+        , Html.Attributes.disabled model.sending
+        , Html.Events.on
+          "input"
+          Html.Events.targetValue
+          (\str -> Signal.message address (UpdateEmail str))
+        ]
+        []
       ]
-      [] ]
-    , Html.div [] [ Html.input
-      [ Html.Attributes.type' "submit"
-      , Html.Attributes.value "Log in"
+    , Html.div []
+      [ Html.input
+        [ Html.Attributes.class "mdl-textfield__input"
+        , Html.Attributes.placeholder "Code"
+        , Html.Attributes.value model.confirmation
+        , Html.Attributes.disabled model.sending
+        , Html.Events.on
+          "input"
+          Html.Events.targetValue
+          (\str -> Signal.message address (UpdateConfirmation str))
+        ]
+        []
       ]
-      [] ]
+    , Html.div []
+      (if model.sending then [ Html.text "sending" ] else [])
+    , Html.div []
+      [ Html.button
+        [ Html.Attributes.type' "submit"
+        , Html.Attributes.class "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored"
+        , Html.Attributes.disabled model.sending
+        ]
+        [ Html.text "Log in" ]
+      ]
     ]
