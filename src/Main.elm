4ed1adc4e25f8dff6bd77bd598eb749d12e6f161
Michael Alexander <beefsack@gmail.com>
2015-08-06 23:30:47 +1000

    Restructured Main to contain auth state at the app level.

--- src/Main.elm
+++ src/Main.elm
@@ -7,17 +7,51 @@ import View.Auth
 import View.Home
 import View.Games
 
-main : Signal Html.Html
-main = Signal.map route History.hash
+type alias Model =
+  { auth : View.Auth.Model
+  , path : String
+  }
 
-actions = Signal.mailbox Nothing
-address = Signal.forwardTo actions.address Just
+type Action
+  = SetPath String
+  | AuthAction View.Auth.Action
+  | NoOp
 
-route : Route Html.Html
-route = match
-  [ ""       :-> View.Home.display
-  , "#auth"  :-> \url -> View.Auth.view address View.Auth.init
-  , "#games" :-> View.Games.display
-  ] display404
+update : Action -> Model -> Model
+update action model =
+  case action of
+    SetPath path ->
+      { model | path <- path }
+    AuthAction authAction ->
+      { model | auth <- View.Auth.update authAction model.auth }
+    NoOp ->
+      model
 
-display404 = always (Html.text "Page not found")
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  let
+    authAddress = Signal.forwardTo address AuthAction
+    display404 _ = Html.text "Page not found"
+    viewPage = match
+      [ "#auth" :-> always (View.Auth.view authAddress model.auth)
+      ] display404
+  in
+    viewPage model.path
+
+setPaths : Signal Action
+setPaths = Signal.map SetPath History.hash
+
+appMailbox : Signal.Mailbox Action
+appMailbox = Signal.mailbox NoOp
+
+actions = Signal.merge setPaths appMailbox.signal
+
+init : Model
+init =
+  { auth = View.Auth.init
+  , path = ""
+  }
+
+main =
+  Signal.map (view appMailbox.address)
+    (Signal.foldp update init actions)
